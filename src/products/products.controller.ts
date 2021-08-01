7ac395e17315b648ad86c1659682b7fb2e823e29
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2021-08-01 15:46:03 +0800

    group all Products endpoints together in swagger

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,11 +7,14 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
+@ApiTags('Products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
